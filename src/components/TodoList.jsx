70da917086df0777eb5a6fca01421b7f4915a166
aryasa22 <saputrawanarya@gmail.com>
2022-06-25 08:34:51 +0800

    add no task text

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -16,25 +16,32 @@ export default function TodoList({
   const appearAdd = () => {
     setIsOpen(true);
   };
+  const isNull = todos.length === 0;
 
   return (
     <div className="todoList">
-      {filteredTodos.map((todo) => {
-        return (
-          <Todo
-            todos={todos}
-            todo={todo}
-            key={todo.id}
-            setTodos={setTodos}
-            setInputTodoTitle={setInputTodoTitle}
-            setInputTodoTime={setInputTodoTime}
-            setIsOpen={setIsOpen}
-            setIsUpdate={setIsUpdate}
-            todoId={todoId}
-            setTodoId={setTodoId}
-          />
-        );
-      })}
+      {isNull ? (
+        <span className="no-task">
+          No task currently, please add a new one!
+        </span>
+      ) : (
+        filteredTodos.map((todo) => {
+          return (
+            <Todo
+              todos={todos}
+              todo={todo}
+              key={todo.id}
+              setTodos={setTodos}
+              setInputTodoTitle={setInputTodoTitle}
+              setInputTodoTime={setInputTodoTime}
+              setIsOpen={setIsOpen}
+              setIsUpdate={setIsUpdate}
+              todoId={todoId}
+              setTodoId={setTodoId}
+            />
+          );
+        })
+      )}
       <button onClick={appearAdd} className="addBtn">
         <FaPlus />
       </button>
